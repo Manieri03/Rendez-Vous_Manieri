d1cc530b72c2b9c83e4eefe3d1cd9556a1367f69
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-21 08:52:21 +0200

    terminato

--- Rendez-Vous_Manieri/Program.cs
+++ Rendez-Vous_Manieri/Program.cs
@@ -5,13 +5,15 @@ namespace Rendez_Vous_Manieri
 {
     class Program
     {
-
+        static int[] v = new int[1000];
+        static int[] w = new int[1000];
+        static int minimo = int.MaxValue;
+        static double media = 0;
         static SemaphoreSlim s1 = new SemaphoreSlim(0);
         static SemaphoreSlim s2 = new SemaphoreSlim(1);
         static void Main(string[] args)
         {
-            int[] v = new int[1000];
-            int[] w = new int[1000];
+
 
             Thread t1 = new Thread(() => Minimo(v));
             Thread t2 = new Thread(() => Media(v,w)); 
@@ -21,6 +23,9 @@ namespace Rendez_Vous_Manieri
             t2.Start();
             while (t1.IsAlive) { }
             while (t2.IsAlive) { }
+
+            Console.WriteLine($"La media è {media}");
+            Console.WriteLine($"Il minimo è {minimo}");
 
             Console.ReadLine();
 
@@ -32,6 +37,15 @@ namespace Rendez_Vous_Manieri
             for(int i=0; i < v.Length; i++)
             {
                 v[i] = r.Next(0, 1000);
+                if (v[i] < minimo)
+                    minimo = v[i];
+            }
+            s2.Release();
+            s1.Wait();
+            for (int i = 0; i < w.Length; i++)
+            {
+                if (w[i] < minimo)
+                    minimo = w[i];
             }
         }
 
@@ -41,7 +55,15 @@ namespace Rendez_Vous_Manieri
             for (int i = 0; i < w.Length; i++)
             {
                 w[i] = r.Next(0, 1000);
+                media += w[i];
             }
+            s1.Release();
+            s2.Wait();
+            for (int i = 0; i < v.Length; i++)
+            {
+                media += v[i];
+            }
+            media = media / v.Length+w.Length;
         }
     }
 }
